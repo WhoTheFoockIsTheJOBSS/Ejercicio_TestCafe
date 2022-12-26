4e7e9f0c18cd3f1eef005efd0c1ac421418d3fbd
WhoTheFoockIsTheJOBSS <104454949+WhoTheFoockIsTheJOBSS@users.noreply.github.com>
2022-12-26 17:23:38 -0600

    Update devicesPAge.js

--- Ejercicio/Pages/devicesPAge.js
+++ Ejercicio/Pages/devicesPAge.js
@@ -2,13 +2,7 @@ import { Selector, t } from "testcafe";
 
  class devicesPage{
 
-    constructor(){
-        this.deviceTypeDD = Selector('select#device_type');
-        this.sortByDD = Selector('select#sort_by');
-        this.addDeviceBtn = Selector('a.submitButton');
-        this.deviceType = Selector('span.device-type');
-        this.deviceCapacity = Selector('span.device-capacity')
-    }
+    constructor(){  }
 
     get device_name(){
         return Selector('.device-info .device-name')
@@ -30,19 +24,8 @@ import { Selector, t } from "testcafe";
         return Selector('button.device-remove')
     };
 
-    async deviceOptionsButtons(id, index){
-        const buttonEdit = this.edit_button().withText(id);
-        const buttonRemove = this.remove_button().withText(index);
-        return(buttonEdit, buttonRemove)
-    };
-
-    async clickOnDeviceType(){
-        await t.click(this.deviceTypeDD);
-        await t.click(Selector("option[value='ALL']"));
-    };
-
-    async clickOnSortBy(){
-        await t.click(this.sortByDD);
+    get addDeviceBtn(){
+        return Selector('a.submitButton')
     };
 
     async clickOnAddDevice(){
